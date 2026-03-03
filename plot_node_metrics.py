ba052d11768797bb10550f978bd715c127f93b71
Hồ Thái Hòa <thaihoa.hn120@gmail.com>
2026-03-03 12:19:54 +0700

    Ready to plot

--- plot_node_metrics.py
+++ plot_node_metrics.py
@@ -65,7 +65,7 @@ if __name__ == "__main__":
     # This node has the best convergence (Loss 0.59 -> 0.08)
     target_file = os.path.join(
         "experiments", 
-        "p2pfl_MNIST_DirichletPartitionStrategy_DFedAdp_model_build_fn_20260216_013607", 
-        "node_0.0.0.0_32797.jsonl"
+        "experiments/p2pfl_MNIST_DirichletPartitionStrategy_DFedAdp_model_build_fn_20260303_044247", 
+        "node_node_1.jsonl"
     )
     plot_jsonl_metrics(target_file)
